828c6913c1588bc98834aa998b579896279a9035
RNfromLabMac <nakano@bi.c.titech.ac.jp>
2024-04-22 16:20:56 +0900

    alertを使わない結果の表示

--- popup.js
+++ popup.js
@@ -1,16 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const dice1 = document.getElementById("1 dice");
-    const dice_1d100 = document.getElementById("1d100");
-  
-    dice1.addEventListener("click", () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "1d6" });
-      });
+document.addEventListener('DOMContentLoaded', function() {
+    var D6 = document.getElementById('1d6');
+    var D100 = document.getElementById('1d100');
+
+    D6.addEventListener('click', function() {
+        var result = getRandomInt(1, 7);
+        document.getElementById('result').textContent = 'Result: ' + result;
     });
 
-    dice_1d100.addEventListener("click", () => {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          chrome.tabs.sendMessage(tabs[0].id, { action: "1d100" });
-        });
-      });
-  });
+    D100.addEventListener('click', function() {
+        var result = getRandomInt(1, 101);
+        document.getElementById('result').textContent = 'Result: ' + result;
+    });
+});
+
+
+// number of dice
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
